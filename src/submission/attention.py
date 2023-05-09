8715d54041780ba599c66125386ef1c534b506a1
Eric Guo <gzw_518@163.com>
2023-05-09 21:40:47 +0800

    Update attention.py
    add SynthesizerAttention

--- src/submission/attention.py
+++ src/submission/attention.py
@@ -96,6 +96,28 @@ class SynthesizerAttention(nn.Module):
         ###       How do these map to the matrices in the handout?
 
         ### START CODE HERE
+        
+        # (B x T x C) is of dimension (batch x block_size x n_embd) which is (batch x l x d) in the handout.
+        # nh should be number_of_heads, and hs would then stand for n_embed (or "dimensionality" d in the handout) per head
+
+        B, T, C = x.size()
+
+        # SynthesizerAttention
+        a = self.w1(x).view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        v = self.value(x).view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+
+        # SynthesizerAttention; Self-attend: (B, nh, T, hs) x (B, nh, hs, T) -> (B, nh, T, T)
+        att = nn.ReLU(x @ a + self.b2) @ self.w2 + self.b2
+        att = att.masked_fill(self.mask[:,:,:T,:T] == 0, -1e10) # todo: just use float('-inf') instead?
+        att = F.softmax(att, dim=-1)
+        att = self.attn_drop(att)
+        y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
+        y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
+
+        # output projection
+        y = self.resid_drop(self.proj(y))
+        return y        
+        
         ### END CODE HERE
 
         raise NotImplementedError
